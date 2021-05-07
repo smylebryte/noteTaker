d519b07cd69ab62fe84cddca9d71561302f2d7a1
Smylebryte <aliciahughes1446@gmail.com>
2021-05-07 11:54:04 -0500

    added env variable

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const apiRoutes = require('./routes/apiRoutes')
 const htmlRoutes = require('./routes/htmlRoutes')
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(express.static('public'))
 app.use('/api', apiRoutes);
